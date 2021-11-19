e00e99920dbffc2d2a27352b3763a5323a37e669
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2021-11-19 18:01:50 -0300

    Card1 Samuel

---
+++ js/scriptSamuel.js
@@ -0,0 +1,79 @@
+// Código JS
+//https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/ID_DO_QUIZZ
+//geral: https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes
+
+const idQuizz = 2;
+let quizzSelecionado; 
+
+function answer(response){
+    quizzSelecionado = response.data
+    console.log(quizzSelecionado);
+    alterarBanner();
+    alteraPergunta();
+    alteraResultado();
+}
+
+function alterarBanner(){
+    const titleQuizz = document.querySelector('.banner-text');
+    titleQuizz.innerHTML = quizzSelecionado.title;
+
+    const imgQuizz = document.querySelector('.banner-topo');
+    imgQuizz.style.backgroundImage = `url(${quizzSelecionado.image})`;
+}
+
+function alteraPergunta(){
+    const arrayPerguntas = quizzSelecionado.questions;
+    const elementoPergunta = document.querySelector('.tela-exibicao-quizz');
+
+    let aux;
+    let exibeOpcoes;
+
+    // console.log(arrayPerguntas)
+
+    for(let i = 0; i < arrayPerguntas.length; i++){
+        // console.log(pergunta)
+        let pergunta = arrayPerguntas[i]
+        aux = `
+        <div class="visualizacao-perguntas">
+            <div class="perguntaj" style="background-color: ${pergunta.color}">
+                <h3 class="pergunta-text">
+                    ${pergunta.title}
+                </h3>
+            </div>
+        `
+        
+        exibeOpcoes = `<div class="opcoes">`
+
+        for (let i = 0; i < pergunta.answers.length; i++) {
+            let alternativa = pergunta.answers[i];
+            // console.log(alternativa)
+
+            exibeOpcoes += `
+                <div class="text-resposta">
+                    <img class="img-pergunta" src="${alternativa.image}" alt="">
+                    <p>${alternativa.text}</p>
+                </div>
+            `
+        }
+        exibeOpcoes += `</div>`
+
+        elementoPergunta.innerHTML += aux + exibeOpcoes + `</div>`
+    }
+}
+
+function alteraResultado(){
+    const arrayResultados = quizzSelecionado.levels;
+    const elementoResultado = document.querySelector('.visualizacao-resultados');
+}
+
+function carregaQuizz(){
+    // const url = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`)
+
+    const url = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`);
+    
+    url.then(answer);
+    url.catch(error => { 
+        return error
+    });
+}
+carregaQuizz()
